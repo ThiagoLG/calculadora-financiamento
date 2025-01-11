943d98636e11a7aba3bdf2976281a0244337db33
ThiagoLG <thiago.lg53@gmail.com>
2025-01-10 23:02:08 -0300

    ✨feat: formulário de financiamento
     - Estrutura do formulário de financiamento, utilizando react hookform e zod para validações.
     - Implementados todos os inputs de parâmetros de cálculos.

--- financial-apps-central/src/app/(pages)/financiamento/_components/formulario/form.tsx
+++ financial-apps-central/src/app/(pages)/financiamento/_components/formulario/form.tsx
@@ -3,20 +3,60 @@ import { useState } from 'react';
 import styles from './form.module.scss';
 import { Controller, useForm } from 'react-hook-form';
 import { CurrencyInput } from 'react-currency-mask';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-interface IFormFinancingData {
-  totalValue: number;
-  downPayment: number;
-  numberOfInstallments: number;
-  anualInterestRate: number;
-  targetPaymentTerm: number;
+const FormFinancingSchema = z
+  .object({
+    totalValue: z.preprocess(preprocessNumber, z.number().min(1000, 'O valor mínimo é de R$ 1.000,00')),
+    downPayment: z.preprocess(preprocessNumber, z.number().min(1, 'O valor da entrada é obrigatório')),
+    numberOfInstallments: z.coerce.number().min(1, 'É necessário ter pelo menos 1 parcela'),
+    anualInterestRate: z.coerce
+      .number()
+      .min(0, 'A taxa de juros não pode ser negativa')
+      .max(100, 'A taxa de juros não pode ser maior que 100'),
+    targetPaymentTerm: z.coerce.number().optional()
+  })
+  .superRefine((data, ctx) => {
+    console.log('Data', data);
+    if (data.targetPaymentTerm !== undefined && data.targetPaymentTerm > data.numberOfInstallments) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['targetPaymentTerm'],
+        message: 'O tempo desejado de quitação não pode exceder o número de parcelas'
+      });
+    }
+    if (data.downPayment > data.totalValue * 0.9) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['downPayment'],
+        message: 'O valor da entrada não pode ser maior que 90% do valor total do imóvel'
+      });
+    }
+  });
+
+type IFormFinancingData = z.infer<typeof FormFinancingSchema>;
+
+function preprocessNumber(val: unknown) {
+  return Number(val) || 0;
 }
+
 export function FormFinancing() {
-  const { register, handleSubmit, formState, control } = useForm<IFormFinancingData>();
+  const { register, handleSubmit, formState, control } = useForm<IFormFinancingData>({
+    resolver: zodResolver(FormFinancingSchema),
+    defaultValues: {
+      totalValue: 300_000,
+      downPayment: 80_000,
+      anualInterestRate: 9,
+      numberOfInstallments: 360,
+      targetPaymentTerm: 60
+    }
+  });
   const [formData, setFormData] = useState<IFormFinancingData | null>(null);
 
   function formSubmit(data: IFormFinancingData) {
     setFormData(data);
+
     console.log('Formulário enviado!', data);
     console.log('Formulário enviado!', formState);
   }
@@ -29,6 +69,7 @@ export function FormFinancing() {
           <Controller
             name="totalValue"
             control={control}
+            data-invalid={formState?.errors?.totalValue ? 'true' : 'false'}
             render={({ field }) => {
               return (
                 <CurrencyInput
@@ -40,6 +81,9 @@ export function FormFinancing() {
               );
             }}
           />
+          {formState?.errors?.totalValue && (
+            <span className={styles.errorAlert}>{formState.errors.totalValue.message}</span>
+          )}
         </div>
 
         <div className={styles.formGroup}>
@@ -47,6 +91,7 @@ export function FormFinancing() {
           <Controller
             name="downPayment"
             control={control}
+            data-invalid={formState?.errors?.downPayment ? 'true' : 'false'}
             render={({ field }) => {
               return (
                 <CurrencyInput
@@ -58,21 +103,48 @@ export function FormFinancing() {
               );
             }}
           />
+          {formState?.errors?.downPayment && (
+            <span className={styles.errorAlert}>{formState.errors.downPayment.message}</span>
+          )}
         </div>
 
         <div className={styles.formGroup}>
           <label>Número de Parcelas</label>
-          <input type="number" {...register('numberOfInstallments', { required: true, min: 12 })} />
+          <input
+            type="number"
+            data-invalid={formState?.errors?.numberOfInstallments ? 'true' : 'false'}
+            {...register('numberOfInstallments')}
+          />
+          {formState?.errors?.numberOfInstallments && (
+            <span className={styles.errorAlert}>{formState.errors.numberOfInstallments.message}</span>
+          )}
         </div>
 
         <div className={styles.formGroup}>
           <label>Taxa de Juros Anual (%)</label>
-          <input type="number" step="0.1" {...register('anualInterestRate', { required: true, min: 0 })} />
+          <input
+            type="number"
+            min="0"
+            max="100"
+            step="0.1"
+            data-invalid={formState?.errors?.anualInterestRate ? 'true' : 'false'}
+            {...register('anualInterestRate')}
+          />
+          {formState?.errors?.anualInterestRate && (
+            <span className={styles.errorAlert}>{formState.errors.anualInterestRate.message}</span>
+          )}
         </div>
 
         <div className={styles.formGroup}>
           <label>Tempo Desejado Quitação</label>
-          <input type="number" {...register('targetPaymentTerm')} />
+          <input
+            type="number"
+            data-invalid={formState?.errors?.targetPaymentTerm ? 'true' : 'false'}
+            {...register('targetPaymentTerm')}
+          />
+          {formState?.errors?.targetPaymentTerm && (
+            <span className={styles.errorAlert}>{formState.errors.targetPaymentTerm.message}</span>
+          )}
         </div>
       </div>
 
